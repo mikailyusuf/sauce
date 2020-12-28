ff2027ad8f02e3d09af0f3b977ac8ad9c49c80df
Mikail Yusuf <mikailkyusuf@gmail.com>
2020-12-28 22:23:53 +0100

    Refactored Urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,13 @@ from api import views
 from api.views import Logout, Login, Record, Tickets, OrderTicket, GetTickets, AddTicket, CreateToken
 
 urlpatterns = [
-    path('addUser/', Record.as_view(), name="register"),
+    path('register_user/', Record.as_view(), name="register"),
     path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout"),
-    path('get_tickets/',GetTickets.as_view(), name="logout"),
-    path('add_ticket/', AddTicket.as_view(), name="logout"),
-    path('order/', OrderTicket.as_view(), name="order"),
-    path('list/<int:pk>', views.getUserTicket),
-    path('createToken', CreateToken.as_view()),
+    path('list_tickets/',GetTickets.as_view(), name="list_tickets"),
+    path('add_ticket/', AddTicket.as_view(), name="add_ticket"),
+    path('order_ticket/', OrderTicket.as_view(), name="order_ticket"),
+    path('get_ticket/<int:pk>', views.getUserTicket),
+    path('createUserToken', CreateToken.as_view()),
 
 ]

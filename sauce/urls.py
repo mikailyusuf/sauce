71603957c92e2b12b028a4a7423cd24e0181a3d3
Mikail Yusuf <mikailkyusuf@gmail.com>
2021-01-13 14:20:45 +0100

    Fix

--- sauce/urls.py
+++ sauce/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth', include('authentication.urls')),
-    path('api/tickets', include('tickets.urls')),
+    path('api/auth/', include('authentication.urls')),
+    path('api/tickets/', include('tickets.urls')),
     url('doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
